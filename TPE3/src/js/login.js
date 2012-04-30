46a13d9bf2221ca7b017719e2d2faf6ec33bb484
framundo <feedeee@hotmail.com>
2012-04-30 19:17:55 -0300

    js test

--- TPE3/src/js/login.js
+++ TPE3/src/js/login.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
 	$('#loginForm').submit(function() {
-		if(!$('#name').val()||!$('#password').val()) {
+		var username=$('#name').val();
+		var password=$('#password').val();
+		if(!username||!password) {
+			alert('Error'+username+password);
+			return false;
+		} else{
+			var sv=new Server();
+			var ans=sv.signIn(username,password);
+			alert('Hello');
 			return false;
 		}
+		alert('asd');
+		return false;
 	});
 });
