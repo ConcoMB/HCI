3ece213d9b335cd76f856d13083e7dc041fa2f56
unknown <Fede@.(none)>
2012-05-01 20:16:48 -0300

    asd

--- TPE3/src/js/login.js
+++ TPE3/src/js/login.js
@@ -1,13 +1,5 @@
 function signIn(username,password){
-	/*DUMMY FUNCTION*/
-	var txt='<response status="ok"><authentication><token>244f6439f45f207f1eb89fb2355a4768</token><user id="1" username="itba" name="ITBA" last_login_date="2009-06-31" /></authentication></response>';
-	/*PARSE*/
-	var xmlDoc = $.parseXML( txt );
-    var $xml = $( xmlDoc );
-   	var status=$xml.find('response').attr('status');
-   	if(status=='ok'){
-   		return {name:$xml.find('user').attr('username')};
-   	}
+	return request('signIn');
 }
 
 function updateUserPanel(name) {
