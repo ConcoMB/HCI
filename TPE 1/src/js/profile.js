96b52f7fc6d9705f6550e196db3e1c309edbe4b3
Vicky Mesa Alcorta <m_vickymesalcorta@hotmail.com>
2012-05-10 17:49:16 -0300

    profile.js y profile.html, no se en donde tengo que hacer el llamado para darle comportamiento al boton -edit profile- que lo que hace es ocultar los span con los datos de la cuenta, ocutal el boton -edit profile-, mostrar los input con los datos de la cuenta, y mostrar el boton aceptar que lo que hace es llamar a la funcion update account

--- TPE 1/src/js/profile.js
+++ TPE 1/src/js/profile.js
@@ -13,3 +13,63 @@ function GetAccount(){
 	}
 	return request("GetAccount", params, 'Security');
 }
+
+function editProfileHandler(){
+	
+	$("#editProfileButton").css('display','none');
+	$(".profData").css('display','none');
+	
+	var name = $("#uname").text();
+	var email = $("#uemail").text();
+	var bdate = $("#ubdate").text();
+	
+	
+	$("#prof_name").attr("value",name);
+	$("#prof_mail").attr("value",email);
+	$("#prof_birthDate").attr("value",bdate);
+	
+	$(".prof_input").css('display','inline');
+	
+	$("#acceptProfileButton").css('display','inline');
+	
+	$("#acceptProfileButton").submit(acceptProfileHandler);
+}
+
+function acceptProfileHandler(){
+		
+	
+	var name = $("#prof_name").attr("value");
+	var email = $("#prof_mail").attr("value");
+	var bdate = $("#prof_bdate").attr("value");
+	var error = false;
+	
+	if(!name){
+		$('#reqProfName').css('display','inline');
+		error = true;
+	}
+	if(!email){
+		$('#reqProfEmail').css('display','inline');
+		error = true;
+	}
+	if(!bdate){
+		$('#reqProfBdate').css('display','inline');
+		error = true;
+	}
+	
+	if(error == false){
+		var xml = '<account><name>' + name + '</name><email>'; 
+		xml += email + '</email><birth_date>' + bdate + '</birth_date><account>';
+		var req=updateAccount(xml);
+	}
+	
+	return false;
+}
+
+function updateAccount(acc){
+	var params={
+		username: $(user).find("user").attr("username"),
+		authentication_token : $(user).find("token").text(),
+		account: acc
+	};
+	return request('UpdateAccount',params,'Security');
+}
