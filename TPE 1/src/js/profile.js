567319653f4bfff383b2c2f2becd762ff51a6517
Vicky Mesa Alcorta <m_vickymesalcorta@hotmail.com>
2012-05-11 01:48:03 -0300

    profile.js

--- TPE 1/src/js/profile.js
+++ TPE 1/src/js/profile.js
@@ -1,86 +1,150 @@
-function viewProfile(){
-	var account=GetAccount();
+function viewProfile() {
+	var account = GetAccount();
 	$("#uname").text($(account).find("name").text());
 	$("#usern").text($(account).find("username").text());
 	$("#uemail").text($(account).find("email").text());
 	$("#ubdate").text($(account).find("birth_date").text());
 	$("#editProfileButton").click(editProfileHandler);
-	$("#cancelEditProfile").click(function(){
+	$("#cancelEditProfile").click(function() {
 		$(window).trigger('hashchange');
 	});
-	
+	$("#changePasswordButton").click(changePassHandler);
+	$("#cancelChangePassword").click(function() {
+		$(window).trigger('hashchange');
+	});
+
 }
 
-function GetAccount(){
-	var params={
-		username: $(user).find("user").attr("username"),
+function GetAccount() {
+	var params = {
+		username : $(user).find("user").attr("username"),
 		authentication_token : $(user).find("token").text()
 	}
 	return request("GetAccount", params, 'Security');
 }
 
-function editProfileHandler(){
-	
-	$("#editProfileButton").css('display','none');
-	$(".profData").css('display','none');
-	
+function editProfileHandler() {
+
+	$("#editProfileButton").css('display', 'none');
+	$(".profData").css('display', 'none');
+
 	var name = $("#uname").text();
 	var email = $("#uemail").text();
 	var bdate = $("#ubdate").text();
-	
-	
-	$("#prof_name").attr("value",name);
-	$("#prof_mail").attr("value",email);
-	$("#prof_birthDate").attr("value",bdate);
-	
-	$(".prof_input").css('display','inline');
-	$("#cancelEditProfile").css('display','inline');
-	$("#acceptProfileButton").css('display','inline');
-	
+
+	$("#prof_name").attr("value", name);
+	$("#prof_mail").attr("value", email);
+	$("#prof_birthDate").attr("value", bdate);
+
+	$(".prof_input").css('display', 'inline');
+	$("#cancelEditProfile").css('display', 'inline');
+	$("#acceptProfileButton").css('display', 'inline');
+
 	$("#prof_update").submit(acceptProfileHandler);
 }
 
-function acceptProfileHandler(){
-		
-	
+function acceptProfileHandler() {
+
 	var name = $("#prof_name").attr("value");
 	var email = $("#prof_mail").attr("value");
 	var bdate = $("#prof_birthDate").attr("value");
 	var error = false;
-	
-	if(!name){
-		$('#reqProfName').css('display','inline');
+
+	if(!name) {
+		$('#reqProfName').css('display', 'inline');
 		error = true;
 	}
-	if(!email){
-		$('#reqProfEmail').css('display','inline');
+	if(!email) {
+		$('#reqProfEmail').css('display', 'inline');
 		error = true;
 	}
-	if(!bdate){
-		$('#reqProfBdate').css('display','inline');
+	if(!bdate) {
+		$('#reqProfBdate').css('display', 'inline');
 		error = true;
 	}
-	
-	if(error == false){
-		alert("ENTRE AL IF1");
-		var xml = '<account><name>' + name + '</name><email>'; 
+
+	if(error == false) {
+
+		var xml = '<account><name>' + name + '</name><email>';
 		xml += email + '</email><birth_date>' + bdate + '</birth_date><account>';
-		var req=updateAccount(xml);
-		if($(req).find('reponse').attr('status') == 'ok'){
-			alert("ENTRE AL IF#@")
+		var req = updateAccount(xml);
+		if($(req).find('response').attr('status') == 'ok') {
+
 			$(window).trigger('hashchange');
 		}
 	}
-	
+
 	return false;
 }
 
-function updateAccount(acc){
-	var params={
-		username: $(user).find("user").attr("username"),
+function updateAccount(acc) {
+	var params = {
+		username : $(user).find("user").attr("username"),
 		authentication_token : $(user).find("token").text(),
-		account: acc
+		account : acc
 	};
-	return request('UpdateAccount',params,'Security');
+	return request('UpdateAccount', params, 'Security');
 }
 
+function changePassHandler() {
+
+	$("#changePasswordButton").css('display', 'none');
+	$(".profDataPass").css('display', 'none');
+
+	$(".prof_inputPass").css('display', 'inline');
+	$(".prof_newPass_item").css('display', 'block');
+
+	$("#acceptChangePassword").css('display', 'inline');
+	$("#cancelChangePassword").css('display', 'inline');
+
+	$("#changePasswordForm").submit(changePasswordHandler);
+
+}
+
+function changePasswordHandler() {
+
+	var pass = $("#prof_pass").attr("value");
+	var newPass = $("#prof_newPass").attr("value");
+	var confPass = $("#prof_confPass").attr("value");
+	var error = false;
+
+	if(!pass) {
+		$('#reqProfPass').css('display', 'inline');
+		error = true;
+	}
+	if(!newPass) {
+		$('#reqProfNewPass').css('display', 'inline');
+		$('#reqProfNewPassMatch').css('display', 'none');
+		error = true;	
+	}
+	if(!confPass) {
+		$('#reqProfConfPass').css('display', 'inline');
+		$('#reqProfNewPassMatch').css('display', 'none');
+		error = true;
+	}
+
+	if(newPass != confPass) {
+		$('#reqProfNewPass').css('display', 'none');
+		$('#reqProfConfPass').css('display', 'none');
+		$('#reqProfNewPassMatch').css('display', 'inline');
+		
+		error = true;
+	}
+
+	if(error == false) {
+		
+		var params = {
+			username : $(user).find("user").attr("username"),
+			password : pass,
+			new_password : newPass
+		}
+		var req = request('ChangePassword', params, 'Security');
+
+		if($(req).find('response').attr('status') == 'ok') {
+			
+			$(window).trigger('hashchange');
+		}
+	}
+
+	return false;
+}
