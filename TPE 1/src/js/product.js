a84f8b97a216ef8bb5793b526d85ec074a02c315
Conrado Mader Blanco <cmaderblanco@gmail.com>
2012-05-17 18:32:01 -0300

    varios

--- TPE 1/src/js/product.js
+++ TPE 1/src/js/product.js
@@ -43,7 +43,7 @@ function getProductListByCategory(cid, p, ord) {
 function updateProduct(pid){
 	var product=GetProduct(pid);
 	$("#like").attr("src","https://www.facebook.com/plugins/like.php?href="+window.location.href);
-	$('#artName').text($(product).find('name').text());
+	$('#artName').html($(product).find('name').text());
 	$('#artPrice').text($(product).find('price').text());
 	$('#artRank').text($(product).find('sales_rank').text());
 	$("#product").data("pid", pid);
