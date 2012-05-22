60d5e8e7d01eb17fb19f25ce5439b95fd15bb67f
Conrado Mader Blanco <cmaderblanco@gmail.com>
2012-05-22 13:16:39 -0300

    map

--- TPE 1/src/js/googleMap.js
+++ TPE 1/src/js/googleMap.js
@@ -6,5 +6,13 @@
       center: latlng,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
+    
     var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  	var marker = new google.maps.Marker({
+      position: latlng, 
+      map: map, 
+      title:"Your package"
+  });   
   }
+
+ 
