eb175903bc92de9396fb8d44fecc5846429aa5e2
unknown <Fede@Fede-PC.(none)>
2012-05-24 15:30:06 -0300

    autoco

--- TPE 1/src/js/search.js
+++ TPE 1/src/js/search.js
@@ -18,7 +18,7 @@ function searchHandler() {
 function search(query, cid, sid, page, order) {
 	$('#searchTitle').text(query);
 	var response = GetProductListByName(query, order, page);
-	$('#numberResults').text($(response).find('products').attr('size')+' ');
+	$('#numberResults').text($(response).find('products').attr('size') + ' ');
 	if(cid != -1) {
 		$(response).find('product').each(function() {
 			if($(this).find('category_id').text() != cid) {
@@ -34,11 +34,40 @@ function search(query, cid, sid, page, order) {
 }
 
 function GetProductListByName(crit, ord, p) {
-	var params={
-		criteria: crit,
-		order: ord,
-		items_per_page: IPP,
-		page: p		
+	var params = {
+		criteria : crit,
+		order : ord,
+		items_per_page : IPP,
+		page : p
 	};
-	return request("GetProductListByName", params,'Catalog');
+	return request("GetProductListByName", params, 'Catalog');
+}
+
+function updateSearchAutocomplete() {
+	var names = new Array();
+	$('#searchCat').find('option').each(function() {
+		var id = $(this).val();
+		if(id >= 0 && id % 1 == 0) {
+			var path = 'service/Catalog.groovy?method=GetProductListByCategory&category_id=' + id + '&language_id=' + $(language).find('language').attr('id');
+			//alert(path);
+			$.ajax({
+				type : "GET",
+				url : path,
+				contentType : "text/html; charset=iso-8859-1",
+				dataType : "xml",
+				success : function(xml) {
+					$(xml).find('name').each(function() {
+						names.push($(this).text());
+					});
+					$('#query').autocomplete({
+						source : names,
+						appendTo : '#autoc',
+						open : function() {
+							$('#input').autocomplete("widget").width(300);
+						}
+					});
+				}
+			});
+		}
+	});
 }
