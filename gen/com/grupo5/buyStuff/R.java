47f667784b5de4e2202c546c49bbbe6f0b2df58c
Conrado Mader Blanco <cmaderblanco@gmail.com>
2012-06-05 12:55:24 -0300

    a

--- gen/com/grupo5/buyStuff/R.java
+++ gen/com/grupo5/buyStuff/R.java
@@ -14,6 +14,10 @@ public final class R {
     }
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f060001;
+        public static final int cream=0x7f060000;
+    }
     public static final class drawable {
         public static final int expander_ic_minimized=0x7f020000;
         public static final int ic_launcher=0x7f020001;
@@ -22,48 +26,48 @@ public final class R {
         public static final int koppe_isologo=0x7f020004;
     }
     public static final class id {
-        public static final int articleText=0x7f060017;
-        public static final int articleThumbnail=0x7f060016;
-        public static final int asd=0x7f060022;
-        public static final int icon=0x7f060018;
-        public static final int image=0x7f060002;
-        public static final int imageView1=0x7f060020;
-        public static final int info1=0x7f06000c;
-        public static final int info2=0x7f06000d;
-        public static final int info3=0x7f06000e;
-        public static final int info4=0x7f06000f;
-        public static final int info5=0x7f060010;
-        public static final int info6=0x7f060011;
-        public static final int info7=0x7f060012;
-        public static final int info8=0x7f060013;
-        public static final int info9=0x7f060015;
-        public static final int label1=0x7f060004;
-        public static final int label2=0x7f060005;
-        public static final int label3=0x7f060006;
-        public static final int label4=0x7f060007;
-        public static final int label5=0x7f060008;
-        public static final int label6=0x7f060009;
-        public static final int label7=0x7f06000a;
-        public static final int label8=0x7f06000b;
-        public static final int label9=0x7f060014;
-        public static final int listText=0x7f060019;
-        public static final int loginButton=0x7f06001f;
-        public static final int logoutButton=0x7f060021;
-        public static final int myArts=0x7f060029;
-        public static final int orderStatus=0x7f060026;
-        public static final int orderStatusLabel=0x7f060024;
-        public static final int orderStatusValue=0x7f060025;
-        public static final int passwordInput=0x7f06001d;
-        public static final int price=0x7f060003;
-        public static final int rememberMe=0x7f06001e;
-        public static final int scroller=0x7f060000;
-        public static final int shippedDateLabel=0x7f060027;
-        public static final int shippedDateValue=0x7f060028;
-        public static final int textInformation=0x7f060023;
-        public static final int textView1=0x7f06001a;
-        public static final int textView2=0x7f06001c;
-        public static final int title=0x7f060001;
-        public static final int usernameInput=0x7f06001b;
+        public static final int articleText=0x7f070017;
+        public static final int articleThumbnail=0x7f070016;
+        public static final int asd=0x7f070022;
+        public static final int icon=0x7f070018;
+        public static final int image=0x7f070002;
+        public static final int imageView1=0x7f070020;
+        public static final int info1=0x7f07000c;
+        public static final int info2=0x7f07000d;
+        public static final int info3=0x7f07000e;
+        public static final int info4=0x7f07000f;
+        public static final int info5=0x7f070010;
+        public static final int info6=0x7f070011;
+        public static final int info7=0x7f070012;
+        public static final int info8=0x7f070013;
+        public static final int info9=0x7f070015;
+        public static final int label1=0x7f070004;
+        public static final int label2=0x7f070005;
+        public static final int label3=0x7f070006;
+        public static final int label4=0x7f070007;
+        public static final int label5=0x7f070008;
+        public static final int label6=0x7f070009;
+        public static final int label7=0x7f07000a;
+        public static final int label8=0x7f07000b;
+        public static final int label9=0x7f070014;
+        public static final int listText=0x7f070019;
+        public static final int loginButton=0x7f07001f;
+        public static final int logoutButton=0x7f070021;
+        public static final int myArts=0x7f070029;
+        public static final int orderStatus=0x7f070026;
+        public static final int orderStatusLabel=0x7f070024;
+        public static final int orderStatusValue=0x7f070025;
+        public static final int passwordInput=0x7f07001d;
+        public static final int price=0x7f070003;
+        public static final int rememberMe=0x7f07001e;
+        public static final int scroller=0x7f070000;
+        public static final int shippedDateLabel=0x7f070027;
+        public static final int shippedDateValue=0x7f070028;
+        public static final int textInformation=0x7f070023;
+        public static final int textView1=0x7f07001a;
+        public static final int textView2=0x7f07001c;
+        public static final int title=0x7f070001;
+        public static final int usernameInput=0x7f07001b;
     }
     public static final class layout {
         public static final int article=0x7f030000;
@@ -81,7 +85,6 @@ public final class R {
         public static final int app_name=0x7f040005;
         public static final int browseItems=0x7f04000c;
         public static final int browserTitle=0x7f04000f;
-        public static final int categories=0x7f040023;
         public static final int categoryTitle=0x7f040009;
         public static final int companyName=0x7f040000;
         public static final int confirmed=0x7f040013;
