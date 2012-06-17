d922bdcee77ef428243b6b53efb7f736ec706eaf
unknown <Fede@Fede-PC.(none)>
2012-06-17 16:59:30 -0300

    icons

--- gen/com/grupo5/buyStuff/R.java
+++ gen/com/grupo5/buyStuff/R.java
@@ -22,10 +22,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int expander_ic_minimized=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int ic_menu_companylogo=0x7f020002;
-        public static final int ic_splash=0x7f020003;
-        public static final int koppe_isologo=0x7f020004;
+        public static final int expander_ic_minimized2=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ic_menu_companylogo=0x7f020003;
+        public static final int ic_splash=0x7f020004;
+        public static final int koppe_isologo=0x7f020005;
     }
     public static final class id {
         public static final int articleText=0x7f080017;
